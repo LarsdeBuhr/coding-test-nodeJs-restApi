17a0c4c54550e3ff884b66e0b282d8ab86d56a3e
Lars <LarsdeBuhr@users.noreply.github.com>
2022-07-25 14:56:14 +0200

    restructuring the code. new function: processUserInput

--- src/app.js
+++ src/app.js
@@ -1,9 +1,7 @@
 import path from "path";
 import express from "express";
-import fetch from "node-fetch";
 import { fileURLToPath } from "url";
-import randomArtistsList from "../json/randomArtists.json" assert { type: "json" };
-import writeToCsv from "./writeToCsv.js";
+import processUserInput from "./processUserInput.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -15,56 +13,15 @@ const app = express();
 const publicDirectoryPath = path.join(__dirname, "../public");
 app.use(express.static(publicDirectoryPath));
 
+app.use(express.urlencoded({ extended: true }));
+app.post("", (req, res) => {
+   //getting user input via post-method
+   processUserInput(req);
+   res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
 //Starts server on environment variable PORT or port 3000
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
    console.log(`Server is running on port ${port}`);
-});
-
-app.use(express.urlencoded({ extended: true }));
-app.post("", (req, res) => {
-   //getting user input via post-method
-   let filenameFromUserInput = encodeURIComponent(req.body.filename) + ".csv";
-   let artistFromUserInput = encodeURIComponent(req.body.artist);
-
-   const apiKey = "6ee9913bbcf31aa24cd6b422e2646f6a";
-   fetch(
-      `https://ws.audioscrobbler.com/2.0/?method=artist.search&artist=${artistFromUserInput}&api_key=${apiKey}&format=json`
-   )
-      .then((response) => response.json())
-      .then((dataFromJSON) => {
-         //Checking if the artist, the user is looking for, is in the dataset
-         let artistInformation =
-            dataFromJSON.results.artistmatches.artist.filter(
-               (artist) =>
-                  encodeURIComponent(artist.name.toLowerCase()) ===
-                  artistFromUserInput.toLowerCase()
-            );
-
-         let data = null;
-         //If the entered artist name is in the JSON file
-         if (artistInformation.length > 0) {
-            data = {
-               name: artistInformation[0].name,
-               mbid: artistInformation[0].mbid,
-               url: artistInformation[0].url,
-               image_small: artistInformation[0].image[0]["#text"],
-               image: JSON.stringify(artistInformation[0].image),
-            };
-         } else {
-            //If the entered artist name is not in the JSON file a static JSON file will be used as a fallback. The artist name will be picked randomly
-            data =
-               randomArtistsList[
-                  Math.floor(Math.random() * randomArtistsList.length)
-               ];
-         }
-
-         //calling the writeToCsv Method with the inputted filename and artist datas
-         writeToCsv(filenameFromUserInput, data);
-         res.sendFile(path.join(__dirname, "../public/index.html"));
-      })
-      //Catching errors
-      .catch((err) => {
-         res.sendFile(path.join(__dirname, "../public/error.html"));
-      });
 });
--- src/writeToCsv.js
+++ src/writeToCsv.js
@@ -1,8 +1,7 @@
 import { fileURLToPath } from "url";
-import fs from 'fs';
-import path from 'path';
-import json2csv from 'json2csv';
-
+import fs from "fs";
+import path from "path";
+import json2csv from "json2csv";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -14,16 +13,16 @@ const writeToCsv = async (fileName, data) => {
    let row;
    // If file doesn't exist, new file will be created and artist information with headers will be added
    if (!fs.existsSync(filename)) {
-      row = json2csv.parse(data, { header: true, delimiter:";"});
+      row = json2csv.parse(data, { header: true, delimiter: ";" });
    } else {
       // If file exists, artist information will be added without headers
-      row = json2csv.parse(data, { header: false, delimiter:";"});
+      row = json2csv.parse(data, { header: false, delimiter: ";" });
    }
-   
+
    // Append file function can create new file too.
    fs.appendFileSync(filename, row);
    // Always add new line if file already exists.
    fs.appendFileSync(filename, "\r\n");
-}
+};
 
 export default writeToCsv;
